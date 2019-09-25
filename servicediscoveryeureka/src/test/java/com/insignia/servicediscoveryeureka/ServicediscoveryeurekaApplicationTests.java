2b3d7510e880db6f57d6ab272469e81d80114ef4
Sthembiso Maseko <37987676+SthePhi@users.noreply.github.com>
2019-09-25 15:18:50 +0200

    Add files via upload

---
+++ servicediscoveryeureka/src/test/java/com/insignia/servicediscoveryeureka/ServicediscoveryeurekaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.insignia.servicediscoveryeureka;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ServicediscoveryeurekaApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
